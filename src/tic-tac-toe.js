c4d4d4e6fcc4ab804dddc87e49c0d0a060cfd359
du-dim <dim-dubovik@yandex.ru>
2021-08-21 22:57:12 +0300

    feat: add solution

--- src/tic-tac-toe.js
+++ src/tic-tac-toe.js
@@ -1,35 +1,71 @@
 class TicTacToe {
-    constructor() {
-
+    constructor() {  
+        this.won = false;
+        this.val;
+        this.row;
+        this.col;        
+        this.mtr = [Array(3).fill(null), Array(3).fill(null), Array(3).fill(null)];
+        this.fin = 0;        
     }
-
-    getCurrentPlayerSymbol() {
-
+  
+    getCurrentPlayerSymbol() {         
+        return (this.fin % 2 === 0)  ? 'x': 'o';                    
     }
+         
+    nextTurn(row, col) { 
+        this.row = row;
+        this.col = col;
+           
+        if ((this.fin % 2 > 0)&&(this.mtr[row][col] === null)) { 
+            this.val = 'o'; 
+        }
+        else if ((this.fin % 2 === 0)&&(this.mtr[row][col] === null))  { 
+            this.val = 'x';
+        }
+        else  { 
+            this.val = this.mtr[row][col];
+        }
 
-    nextTurn(rowIndex, columnIndex) {
+        this.mtr[this.row][this.col] = this.val;                
+         
+        this.fin = this.mtr.map(b => b.map(a => (a === 'x')||(a === 'o') ? 1 : 0).reduce((sum, a) => sum + a)).reduce((sum, a) => sum + a); 
+        this.getWinner();         
+    }   
 
-    }
-
-    isFinished() {
-
+    isFinished() {    
+        return ((this.fin === 9) || (this.won === true)) ? true : false;
     }
 
     getWinner() {
-
+        let j = 0;
+        const win = ['x', 'o'];
+        while (j < 2) {
+            for (let i = 0; i < 3; i++) {
+                if ((this.mtr[i][0] === win[j] && this.mtr[i][1] === win[j] && this.mtr[i][2] === win[j]) ||
+                    (this.mtr[0][i] === win[j] && this.mtr[1][i] === win[j] && this.mtr[2][i] === win[j]) || 
+                    (this.mtr[0][0] === win[j] && this.mtr[1][1] === win[j] && this.mtr[2][2] === win[j]) ||
+                    (this.mtr[2][0] === win[j] && this.mtr[1][1] === win[j] && this.mtr[0][2] === win[j])) {
+                        this.won = true;
+                        return win[j];
+                }
+                else false;                                      
+            }
+            j++; 
+        }
+        return null;
     }
 
-    noMoreTurns() {
-
+    noMoreTurns() {        
+        return this.fin === 9 ? true : false;
     }
 
     isDraw() {
-
+        return ((this.won === false) && (this.fin === 9)) ? true: false;         
     }
 
-    getFieldValue(rowIndex, colIndex) {
-
-    }
+    getFieldValue(r, c) {
+        return this.mtr[r][c];
+    }    
 }
 
 module.exports = TicTacToe;
